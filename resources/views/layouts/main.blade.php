20f5ce93ece0f58b9ab5f4f35a415c9dc80cc8db
sodaler <misha_lector87@mail.ru>
2022-07-23 03:25:13 +0400

    CRUD for categories, tags, colors

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -158,7 +158,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('category.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-clone"></i>
                             <p>
                                 Categories
@@ -166,7 +166,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('tag.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-tags"></i>
                             <p>
                                 Tags
@@ -174,7 +174,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('color.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-fill-drip"></i>
                             <p>
                                 Colors
