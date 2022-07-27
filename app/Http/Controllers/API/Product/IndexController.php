d8ecb6619ba075ea01ce16556abc179f82583532
sodaler <misha_lector87@mail.ru>
2022-07-27 19:27:14 +0400

    additional changes for index

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API\Product;
 
 
-use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\IndexProductResource;
 use App\Models\Product;
 use Illuminate\Routing\Controller;
 
@@ -12,6 +12,6 @@ class IndexController extends Controller
     public function __invoke()
     {
         $products = Product::all();
-        return ProductResource::collection($products);
+        return IndexProductResource::collection($products);
     }
 }
